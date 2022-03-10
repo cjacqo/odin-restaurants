ee71c00a153b83f14fbb3a9974cb32b6f0258fbe
cjacqo <contact.jayden.cipriano@gmail.com>
2022-03-10 02:29:40 -0700

    Added a module and rule to import image assets from the src folder

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,8 +1,15 @@
 import Header from '../layout/Header'
+import Img from '../imgs/img.jpg'
 
 const Dashboard = () => {
     const dashboard = document.createElement('div')
+
+    // @@@TEST: practice loading assets
+    const myImg = new Image()
+    myImg.src = Img
+    
     dashboard.appendChild(Header())
+    dashboard.appendChild(myImg)
     return dashboard
 }
 
