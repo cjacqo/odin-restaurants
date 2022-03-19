11283ea3be12c071aad92e2871e00cc3a4cfc88d
cjacqo <contact.jayden.cipriano@gmail.com>
2022-03-18 23:10:35 -0700

    Added comments to the TitleBox.js element component

--- src/elements/TitleBox.js
+++ src/elements/TitleBox.js
@@ -1,18 +1,22 @@
 const TitleBox = (title, subtitle) => {
+    // Create container elements
     const container = document.createElement('div')
-    container.classList.add('text-container')
-
     const titleText = document.createElement('h1')
     const subtitleText = document.createElement('h6')
+    // Add class names
+    container.classList.add('text-container')
     titleText.classList.add('title-text')
     subtitleText.classList.add('sub-title-text')
-
+    // Insert title to the titleText element and append
     titleText.innerText = title
     container.appendChild(titleText)
+    // Check if a subtitle exists
+    //      TRUE: insert subtitle to the subtitleText element and append
     if (subtitle) {
         subtitleText.innerText = subtitle
         container.appendChild(subtitleText)
     }
+    // Return container
     return container
 }
 
